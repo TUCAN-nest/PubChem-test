608b534734b3b9d7a7c7c614345823e3148dbdac
Jan C. Brammer <jan.c.brammer@gmail.com>
2023-05-17 13:18:16 +0000

    Write logs to SQLite

--- pipeline.py
+++ pipeline.py
@@ -1,10 +1,15 @@
 import multiprocessing
 import gzip
-from time import ctime
+import sqlite3
+from datetime import datetime
 from typing import Callable, Iterator
 
 
-def read_molfiles_from_zipped_sdf(sdf_path: str) -> Iterator[str]:
+def get_current_time():
+    return datetime.now().isoformat(timespec="seconds")
+
+
+def _read_molfiles_from_zipped_sdf(sdf_path: str) -> Iterator[str]:
     """Generator yielding molfiles from gzipped SDF file.
     (G)unzips the SDF on a line-by-line basis to avoid loading entire SDF into memory.
 
@@ -23,56 +28,63 @@ def read_molfiles_from_zipped_sdf(sdf_path: str) -> Iterator[str]:
                 current_molfile += line
 
 
-def produce_molfiles(
+def _produce_molfiles(
     molfile_queue: multiprocessing.Queue, sdf_path: str, n_poison_pills: int
 ):
-    for molfile in read_molfiles_from_zipped_sdf(sdf_path):
+    for molfile in _read_molfiles_from_zipped_sdf(sdf_path):
         molfile_queue.put(molfile)
 
     for _ in range(n_poison_pills):
         molfile_queue.put("DONE")  # poison pill: tell consumer processes we're done
 
-    print(f"{ctime()}: Done producing molfiles.")
+    print(f"{get_current_time()}: Done producing molfiles.")
 
 
-def consume_molfiles(
+def _consume_molfiles(
     molfile_queue: multiprocessing.Queue,
     result_queue: multiprocessing.Queue,
-    consumer_functions: list[Callable],
+    consumer_function: Callable,
+    get_molfile_id: Callable,
     process_id: int,
 ):
     for molfile in iter(molfile_queue.get, "DONE"):
-        for consumer_function in consumer_functions:
-            result_queue.put(consumer_function(molfile))
+        result_queue.put(consumer_function(molfile, get_molfile_id))
 
     result_queue.put(f"DONE")
 
-    print(f"{ctime()}: Process {process_id} done consuming molfiles.")
+    print(f"{get_current_time()}: Process {process_id} done consuming molfiles.")
 
 
 def run(
     sdf_path: str,
-    log_path: str,
-    consumer_functions: list[Callable],
+    log_db: sqlite3.Connection,
+    consumer_function: Callable,
+    get_molfile_id: Callable,
     number_of_consumer_processes: int,
 ):
     molfile_queue: multiprocessing.Queue = multiprocessing.Queue()  # TODO: limit size?
     result_queue: multiprocessing.Queue = multiprocessing.Queue()
 
-    print(f"{ctime()}: Starting producer process.")
+    print(f"{get_current_time()}: Starting producer process.")
     producer_process = multiprocessing.Process(
-        target=produce_molfiles,
+        target=_produce_molfiles,
         args=(molfile_queue, sdf_path, number_of_consumer_processes),
     )
     producer_process.start()
 
     print(
-        f"{ctime()}: Distributing consumer function over {number_of_consumer_processes} processes."
+        f"{get_current_time()}: Distributing consumer function over {number_of_consumer_processes} processes."
     )
     consumer_processes = [
         multiprocessing.Process(
-            target=consume_molfiles,
-            args=(molfile_queue, result_queue, consumer_functions, process_id),
+            target=_consume_molfiles,
+            args=(
+                molfile_queue,
+                result_queue,
+                consumer_function,
+                get_molfile_id,
+                process_id,
+            ),
         )
         for process_id in range(number_of_consumer_processes)
     ]
@@ -80,18 +92,13 @@ def run(
         consumer_process.start()
 
     number_of_finished_consumer_processes = 0
-    with open(log_path, "w") as log:
-        print(f"{ctime()}: Starting logging to {log_path}.")
-        log.write("time\tmolfile_id\targument\tresult\n")
-        while number_of_finished_consumer_processes < number_of_consumer_processes:
-            result = result_queue.get()  # blocks until result is available
-            if result == "DONE":
-                number_of_finished_consumer_processes += 1
-                continue
-            log.write(f"{ctime()}")
-            for value in result.values():
-                log.write(f"\t{value}")
-            log.write("\n")
+    print(f"{get_current_time()}: Starting logging results.")
+    while number_of_finished_consumer_processes < number_of_consumer_processes:
+        result = result_queue.get()  # blocks until result is available
+        if result == "DONE":
+            number_of_finished_consumer_processes += 1
+            continue
+        log_db.execute("INSERT INTO results VALUES (?, ?, ?, ?)", result)
 
     # processes won't join before all queues their interacting with are empty
     producer_process.join()
